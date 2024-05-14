5a577ca6a14448975d09094fce429902354a2eeb
Mit Joshi <mitjoshi0207@gmail.com>
2024-05-14 13:52:31 +0530

    all done

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:8000',
-    //     changeOrigin: true,
-    //     rewrite: (Path) => Path.replace(/^\/api/, ''),
-    //   },
-    // },
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (Path) => Path.replace(/^\/api/, ""),
+      },
+    },
   },
 });
